cf8a26abd85a70cc612a066dd88c0be8190bc695
gabiwr <113852389+gabiwr@users.noreply.github.com>
2022-09-18 21:48:14 +0300

    Add files via upload

--- Rests-App.py
+++ Rests-App.py
@@ -18,9 +18,9 @@ from pandas.api.types import (
 st.set_page_config(page_title='Restaurantes Rio de Janeiro',
                    layout='centered', initial_sidebar_state='auto')
 
-path = 'C:\\Users\\gabir\\Documents\\Python-Projects\\Restaurantes-Rio\\Rests-Rio.xlsx'
+# path = 'C:\\Users\\gabir\\Documents\\Python-Projects\\Restaurantes-Rio\\Rests-Rio.xlsx'
 
-RestaurantsDB = pd.read_excel(path, sheet_name='DB')
+RestaurantsDB = pd.read_excel('Rests-Rio.xlsx', sheet_name='DB')
 
 RestaurantsDB.set_index('Nome', inplace=True)
 
@@ -255,7 +255,6 @@ if choice == 'Aleatório':
         st.subheader(f'O restaurante escolhido foi {Aleatorio.index[index]}')
         st.dataframe(Aleatorio.iloc[index,:], width=500)
 
-
 # ------------------------------------------------------------------------
 
 if choice == 'Mapa':
@@ -267,81 +266,3 @@ if choice == 'Mapa':
     # fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
 
     st.plotly_chart(fig)
-
-
-
-
-# ------------------------------------------------------------------------
-
-# def filter_dataframe(df: pd.DataFrame) -> pd.DataFrame:
-#     """
-#     Adds a UI on top of a dataframe to let viewers filter columns
-
-#     Args:
-#         df (pd.DataFrame): Original dataframe
-
-#     Returns:
-#         pd.DataFrame: Filtered dataframe
-#     """
-
-
-#     df = df.copy()
-
-#     # Try to convert datetimes into a standard format (datetime, no timezone)
-#     for col in df.columns:
-#         if is_object_dtype(df[col]):
-#             try:
-#                 df[col] = pd.to_datetime(df[col])
-#             except Exception:
-#                 pass
-#         if is_datetime64_any_dtype(df[col]):
-#             df[col] = df[col].dt.tz_localize(None)
-
-#     modification_container = st.container()
-
-#     with modification_container:
-#         to_filter_columns = st.multiselect("Filtrar opções por: ", df.columns)
-#         for column in to_filter_columns:
-#             left, right = st.columns((1, 20))
-#             # Treat columns with < 10 unique values as categorical
-#             if is_categorical_dtype(df[column]) or df[column].nunique() < 10:
-#                 user_cat_input = right.multiselect(
-#                     f"Restaurantes para {column}",
-#                     df[column].unique(),
-#                     default=list(df[column].unique()),
-#                 )
-#                 df = df[df[column].isin(user_cat_input)]
-#             elif is_numeric_dtype(df[column]):
-#                 _min = float(df[column].min())
-#                 _max = float(df[column].max())
-#                 step = (_max - _min) / 100
-#                 user_num_input = right.slider(
-#                     f"Restaurantes para {column}",
-#                     min_value=_min,
-#                     max_value=_max,
-#                     value=(_min, _max),
-#                     step=step,
-#                 )
-#                 df = df[df[column].between(*user_num_input)]
-#             elif is_datetime64_any_dtype(df[column]):
-#                 user_date_input = right.date_input(
-#                     f"Restaurantes para {column}",
-#                     value=(
-#                         df[column].min(),
-#                         df[column].max(),
-#                     ),
-#                 )
-#                 if len(user_date_input) == 2:
-#                     user_date_input = tuple(map(pd.to_datetime, user_date_input))
-#                     start_date, end_date = user_date_input
-#                     df = df.loc[df[column].between(start_date, end_date)]
-#             else:
-#                 user_text_input = right.text_input(
-#                     f"Substring or regex in {column}",
-#                 )
-#                 if user_text_input:
-#                     df = df[df[column].astype(str).str.contains(user_text_input)]
-
-#     return df
-
-
